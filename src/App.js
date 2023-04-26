0e39cdc6c06d5bdf00e7b339453e01431b3fb6cb
atuls9 <atul.sooryawanshi@gmail.com>
2023-04-26 17:56:22 +0530

    adding Cart component as a modal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Store from "./components/Store";
+import Header from "./components/Header";
+import Cart from "./components/Cart";
 
 function App() {
+  const [modalShow, setModalShow] = useState(false);
+  const showModal = () => {
+    setModalShow(true);
+  };
+  // const CloseModal = () => {
+  //   setModalShow(false)
+  // }
   return (
     <div>
+      <Header showModal={showModal} />
+      <Cart show={modalShow} onHide={() => setModalShow(false)} />
       <Store />
-      {/* <h2>app </h2>
-      <Button variant="primary">Primary</Button>{" "}
-      <Button variant="secondary">Secondary</Button>{" "} */}
     </div>
   );
 }
